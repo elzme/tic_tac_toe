6549218284326cedd236aaa89ccf1e58eae95082
Elizabeth Engelman <emengelman@gmail.com>
2014-07-02 10:35:50 -0500

    converts player input from a string to an int

--- lib/player.ex
+++ lib/player.ex
@@ -1,15 +1,17 @@
 defmodule Player do
+  import String
   defstruct type: :human, mark: "X"
 
-  def get_move(player) do
+  def get_move(player, current_board) do
     if player.type == :human do
-      CommandLineIO.gets "Please enter your move (1 - 9):\n"
+      position = CommandLineIO.gets "Please enter your move (1 - 9):\n"
+      to_integer(rstrip(position))
     else
-      get_computer_player_move
+      get_computer_player_move(current_board)
     end
   end
 
-  def get_computer_player_move do
-    "9"
+  def get_computer_player_move(current_board) do
+    String.to_integer("8")
   end
 end
--- test/player_test.exs
+++ test/player_test.exs
@@ -3,13 +3,19 @@ defmodule PlayerTest do
   import ExUnit.CaptureIO
 
   test "it gets the human's move when they are the current player" do
+    current_board = ["", "", "",
+                     "X", "", "",
+                     "", "", ""]
     assert capture_io("1", fn ->
-      input = Player.get_move(%Player{})
+      input = Player.get_move(%Player{}, current_board)
       IO.write input
     end) == "Please enter your move (1 - 9):\n1"
   end
 
   test "it gets the computers move when it is the current player" do
-    assert Player.get_move(%Player{type: :computer, mark: "O"}) == "9"
+    current_board = ["X", "X", "X",
+                     "X", "X", "X",
+                     "X", "X", ""]
+    assert Player.get_move(%Player{type: :computer, mark: "O"}, current_board) == 8
   end
 end
