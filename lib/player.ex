10d795827a07422a2d63c8a9399cf1c7bba927ba
Elizabeth Engelman <emengelman@gmail.com>
2014-07-01 21:53:33 -0500

    adds struct to player module

--- lib/player.ex
+++ lib/player.ex
@@ -1,5 +1,15 @@
 defmodule Player do
-  def get_move do
-    CommandLineIO.gets "Please enter your move (1 - 9):\n"
+  defstruct type: :human, mark: "X"
+
+  def get_move(player) do
+    if player.type == :human do
+      CommandLineIO.gets "Please enter your move (1 - 9):\n"
+    else
+      get_computer_player_move
+    end
+  end
+
+  def get_computer_player_move do
+    "9"
   end
 end
--- test/player_test.exs
+++ test/player_test.exs
@@ -2,10 +2,14 @@ defmodule PlayerTest do
   use ExUnit.Case
   import ExUnit.CaptureIO
 
-  test "it gets the human player's move" do
+  test "it gets the human's move when they are the current player" do
     assert capture_io("1", fn ->
-      input = Player.get_move
+      input = Player.get_move(%Player{})
       IO.write input
     end) == "Please enter your move (1 - 9):\n1"
+  end
+
+  test "it gets the computers move when it is the current player" do
+    assert Player.get_move(%Player{type: :computer, mark: "O"}) == "9"
   end
 end
