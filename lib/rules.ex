cde0368428ab650ce6f4f9af957c9c8eb93137f2
Elizabeth Engelman <emengelman@gmail.com>
2014-07-02 15:13:03 -0500

    adds rules to determine if there's a winner, and ends the game if there is

--- lib/rules.ex
+++ lib/rules.ex
@@ -1,8 +1,9 @@
 defmodule Rules do
   import String
+  @winning_lines [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
   def game_over?(current_board) do
-    !(Board.blank_spaces_left?(current_board))
+    !(Board.blank_spaces_left?(current_board)) || Rules.winner_on_board?(current_board)
   end
 
   def valid_move?(current_board, move) do
@@ -14,6 +15,20 @@ defmodule Rules do
     (move in ["0", 0, "1", 1, "2", 2, "3", 3, "4", 4, "5", 5, "6", 6, "7", 7, "8", 8]) &&
     (Board.open_position?(current_board, move))
   end
+
+  def winner_in_line?(current_board, line) do
+    current_line = [Enum.fetch!(current_board, Enum.fetch!(line, 0)), Enum.fetch!(current_board, Enum.fetch!(line,1)), Enum.fetch!(current_board, Enum.fetch!(line, 2))]
+    if Enum.fetch!(current_board, List.first(line))  == "" do
+      false
+    else
+      Enum.all?(current_line, fn(x) -> x == List.first(current_line) end)
+    end
+  end
+
+  def winner_on_board?(current_board) do
+    Enum.map(@winning_lines, fn(x) -> winner_in_line?(current_board, x) end)
+    |> Enum.any?(&(&1 == true))
+   end
 
   defp convert_potential_move_to_int(move) do #if it is not a valid input, return 100, which will fail the valid_move checker
     try do
--- test/rules_test.exs
+++ test/rules_test.exs
@@ -8,6 +8,13 @@ defmodule RulesTest do
     assert Rules.game_over?(current_board) == true
   end
 
+  test "#game_over?: returns true there is a winner" do
+    current_board = ["X", "X", "O",
+                     "X", "O", "",
+                     "O", "O", "X"]
+    assert Rules.game_over?(current_board) == true
+  end
+
   test "#game_over?: returns false if there are still spaces available" do
     current_board = ["X", "X", "O",
                      "X", "", "X",
@@ -56,4 +63,52 @@ defmodule RulesTest do
                      "O", "O", "X"]
     assert Rules.valid_move?(current_board, "hey") == false
   end
+
+  test "#winner_in_line?: returns false if the given line is all empty spaces" do
+    line = [0, 1, 2]
+    current_board = ["", "", "",
+                     "X", "", "X",
+                     "O", "O", "X"]
+    assert Rules.winner_in_line?(current_board, line) == false
+  end
+
+  test "#winner_in_line?: returns true if the given line is a winner" do
+    line = [0, 1, 2]
+    current_board = ["X", "X", "X",
+                     "X", "", "X",
+                     "O", "O", "X"]
+    assert Rules.winner_in_line?(current_board, line) == true
+  end
+
+  test "#winner_in_line?: returns false if the line is not a winner (contains some empty spaces)" do
+    line = [0, 1, 2]
+    current_board = ["X", "", "X",
+                     "X", "", "X",
+                     "O", "O", "X"]
+    assert Rules.winner_in_line?(current_board, line) == false
+  end
+
+  test "#winner_in_line?: returns false if the line is not a winner (contains no empty spaces)" do
+    line = [0, 1, 2]
+    current_board = ["X", "O", "X",
+                     "X", "", "X",
+                     "O", "O", "X"]
+    assert Rules.winner_in_line?(current_board, line) == false
+  end
+
+  test "#winner_in_line?: true if winner is diagonal" do
+    line = [0, 4, 8]
+    current_board = ["X", "O", "X",
+                     "X", "X", "X",
+                     "O", "O", "X"]
+    assert Rules.winner_in_line?(current_board, line) == true
+  end
+
+  test "#winner_on_board?: returns true if there is a winner anywhere on the board" do
+    current_board = ["X", "X", "X",
+                     "X", "", "X",
+                     "O", "O", "X"]
+    assert Rules.winner_on_board?(current_board) == true
+  end
+
 end
