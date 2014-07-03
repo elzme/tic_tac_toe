cc8525bb044edd1518655f3a35a92b31ad970b5a
Elizabeth Engelman <emengelman@gmail.com>
2014-07-03 16:27:58 -0500

    refactors out negamax logic from player into it's own module

--- lib/player.ex
+++ lib/player.ex
@@ -23,8 +23,8 @@ defmodule Player do
     end
   end
 
-  def get_unbeatable_computer_player_move(current_board, computer_mark) do
-    get_best_move(current_board, computer_mark)
+  defp get_unbeatable_computer_player_move(current_board, computer_mark) do
+    Negamax.get_best_move(current_board, computer_mark)
   end
 
   defp get_human_player_move(player, current_board) do
@@ -34,47 +34,5 @@ defmodule Player do
     else
       get_human_player_move(player, current_board)
     end
-  end
-
-  def get_score(current_board, player_mark) do
-    winner = Rules.who_wins?(current_board)
-    cond do
-      winner == player_mark ->
-        100
-      (winner != player_mark && Rules.winner_on_board?(current_board)) ->
-       -100
-      true ->
-        0
-    end
-  end
-
-  def switch_player_mark(current_player_mark) do
-    if current_player_mark == "X" do
-      "O"
-    else
-      "X"
-    end
-  end
-
-  def score_move(current_board, player_mark, move, depth) do
-    updated_board = Board.update(current_board, move, player_mark)
-    updated_depth = depth + 1
-    if Rules.game_over?(updated_board) do
-      score_divided_by_depth = div(get_score(updated_board, player_mark), updated_depth)
-      [score_divided_by_depth, move]
-    else
-      #[score, depth, move]
-       [(-1 * (List.first(negamax(updated_board, switch_player_mark(player_mark), updated_depth)))), updated_depth, move]
-    end
-  end
-
-  def negamax(current_board, player_mark, depth) do
-    moves = Board.get_blank_spaces(current_board)
-    Enum.map(moves, fn(x) -> score_move(current_board, player_mark, x, depth) end)
-    |>Enum.max()
-  end
-
-  def get_best_move(current_board, computer_mark) do
-    List.last(negamax(current_board, "O", 0))
   end
 end
---
+++ test/negamax_test.exs
@@ -0,0 +1,76 @@
+defmodule NegamaxTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  test "#get_score: returns a 1 if the current player wins" do
+    current_board = ["X", "X", "X",
+                     "", "", "",
+                     "", "", ""]
+    assert Negamax.get_score(current_board, "X") == 100
+  end
+
+  test "#get_score: returns a -1 if the current player loses" do
+    current_board = ["X", "X", "X",
+                     "", "", "",
+                     "", "", ""]
+    assert Negamax.get_score(current_board, "O") == -100
+  end
+
+  test "#get_score: returns a 0 if the game ends in a tie" do
+    current_board = ["X", "O", "X",
+                     "X", "O", "X",
+                     "O", "X", "O"]
+    assert Negamax.get_score(current_board, "O") == 0
+  end
+
+  test "#score_move returns a list with the score and the move if the move causes the game to be over" do
+    current_board = ["X", "X", "",
+                     "", "", "",
+                     "", "", ""]
+    assert Negamax.score_move(current_board, "X", 2, 0) == [100, 2]
+  end
+
+  test "#switch_player_mark: returns `X` if `O` is the current player's mark" do
+    assert Negamax.switch_player_mark("X") == "O"
+  end
+
+  test "#switch_player_mark: returns `O` if `X` is the current player's mark" do
+    assert Negamax.switch_player_mark("O") == "X"
+  end
+
+  test "#get_best_move: it takes the winning move for the computer" do
+    current_board = ["X", "O", "",
+                     "O", "O", "",
+                     "X", "X", "O"]
+    assert Negamax.get_best_move(current_board, "O") == 5
+  end
+
+
+  test "#get_best_move: it takes the tying move instead of losing" do
+    current_board = ["X", "X", "",
+                     "O", "X", "",
+                     "X", "O", "O"]
+    assert Negamax.get_best_move(current_board, "O") == 2
+  end
+
+  test "#get_best_move: it takes the winning move instead of tying" do
+    current_board = ["X", "X", "",
+                     "O", "O", "",
+                     "X", "O", "O"]
+    assert Negamax.get_best_move(current_board, "O") == 5
+  end
+
+  test "#get_best_move: it blocks the opponent" do
+    current_board = ["X", "", "",
+                     "", "X", "",
+                     "", "O", ""]
+    assert Negamax.get_best_move(current_board, "O") == 8
+  end
+
+  test "#get_best_move: gets the quickest win" do
+    current_board = ["X", "", "",
+                     "O", "O", "",
+                     "X", "", ""]
+    assert Negamax.get_best_move(current_board, "O") == 5
+  end
+end
