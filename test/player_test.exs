682a4a31018bd84fd6ca84a74213cfe3affe017d
Elizabeth Engelman <emengelman@gmail.com>
2014-07-03 14:37:02 -0500

    changes current_board in get_move test so it is quicker (with the negmax algorithm)

--- test/player_test.exs
+++ test/player_test.exs
@@ -12,13 +12,13 @@ defmodule PlayerTest do
     end) == "Please enter your move (1 - 9):\n1"
   end
 
-  test "#get_move: gets the computers move when it is the current player" do
-    current_board = ["", "", "",
-                     "", "", "",
-                     "", "", ""]
-    computer_move = Player.get_move(%Player{type: :computer, mark: "O"}, current_board)
-    assert is_integer(computer_move) == true
-  end
+    test "#get_move: gets the computers move when it is the current player" do
+      current_board = ["X", "O", "X",
+                       "X", "O", "X",
+                       "O", "X", ""]
+      computer_move = Player.get_move(%Player{type: :computer, mark: "O"}, current_board)
+      assert is_integer(computer_move) == true
+    end
 
   test "#get_score: returns a 1 if the current player wins" do
     current_board = ["X", "X", "X",
